e86dff119113812355be0423eb733f98fd88dda5
ingAyechsalim <ellord.salim@gmail.com>
2018-08-16 16:06:07 +0100

    react

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+const Salim = () => 
+ (<div className='Credi-card'>
+    <h1 className="Creditname">   CREDIT CARD</h1>
+    <h1 className='Serial-Number'> 1234 1234 1234 1234</h1>
+    <p className='Number-V'><strong>1234</strong></p>
+    <p className='validate'><strong>V: 12/05</strong></p>
+    <p className='Holder' ><strong> CARDHOLDER</strong></p>
+    </div>)
+
+export default  Salim  
